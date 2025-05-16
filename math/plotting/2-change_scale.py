1e1861f8692d299131b0cca12c44fdc17931bed9
David Dau <d.cyubahiro@alustudent.com>
2025-05-16 12:04:25 +0200

    The change of scale

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,14 @@
+#!usr/bin/venv python3
+import numpy as np
+import matplotlib.pyplot as plt
+x =  np.arange(0, 28651, 5730)
+r = np.log(0.5)
+t = 5730
+y = np.exp((r / t) * x)
+plt.plot(x,y)
+plt.title("Exponential Decay of C-14")
+plt.xlabel("Time (years)")
+plt.ylabel("Fraction Reamining")
+plt.yscale("log")
+plt.xlim(0,28650)
+plt.show()
