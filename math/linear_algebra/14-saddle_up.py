e51b8c1a7bf76a3a5cd3afe9a63315472e7db4ca
David Dau <d.cyubahiro@alustudent.com>
2025-05-23 22:21:04 +0200

    Re-done

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -4,5 +4,5 @@ import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """Perform matrix multiplication between mat1 and mat2 in that order"""
+    """Perform matrix multiplication between mat1 and mat2 in that order."""
     return np.matmul(mat1, mat2)
