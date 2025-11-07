4b467b3d33b2732ddc5ddfbdf42dd8803ff9423c
David Dau <d.cyubahiro@alustudent.com>
2025-11-07 08:20:43 +0200

    retry2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python3
-"""Return launch location from SpaceX API"""
+
+
+""" Return list of ships"""
 
 import requests
 import sys
 import time
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./2-user_location.py <URL>")
-        sys.exit(1)
 
+if __name__ == "__main__":
     res = requests.get(sys.argv[1])
 
     if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
         current_time = int(time.time())
         diff = (rate_limit - current_time) // 60
-        print(f"Reset in {diff} min")
+        print("Reset in {} min".format(diff))
+        # get remaining rate
 
     elif res.status_code == 404:
         print("Not found")
-
     elif res.status_code == 200:
-        data = res.json()
-
-        # SpaceX launches have a launchpad ID, we can use it to get the location
-        if "launchpad" in data:
-            launchpad_id = data["launchpad"]
-            launchpad_url = f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}"
-            launchpad_data = requests.get(launchpad_url).json()
-            print(launchpad_data["locality"])  # prints e.g., "Cape Canaveral"
-        else:
-            print("No launchpad info found")
+        res = res.json()
+        print(res['location'])
